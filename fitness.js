9238f84a1fd4136390a8c5dad83af248a75d6205
zahidsammy <120346801+zahidsammy@users.noreply.github.com>
2023-03-25 21:47:29 +0000

    Add files via upload

---
+++ fitness.js
@@ -0,0 +1,10 @@
+function openSignupForm() {
+    document.getElementById("signup-form").style.display = "block";
+}
+
+function closeSignupForm() {
+    document.getElementById("signup-form").style.display = "none";
+}
+
+
+
